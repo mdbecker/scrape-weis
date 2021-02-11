34011ba8f629f9ff1329b25e5b5bc4f18657ab40
Michael Becker <mdbecker@users.noreply.github.com>
2021-02-11 11:00:45 -0500

    Create sleep.py

---
+++ sleep.py
@@ -0,0 +1,15 @@
+from time import time, sleep
+import sys
+
+times = [0, 15, 30, 45]
+x = sys.argv[1
+start = time()
+next_t = min((abs(a-x), a) for a in times)
+if next_t[0] < 3:
+    exit(0)
+sleep_t = min((abs(a-x), a) for a in times if a-x >=0)[1]
+sleep_to = start + (60*(sleep_t-x))
+while time() < sleep_to:
+    sleep_for = (sleep_to - time())/2
+    print(f'{sleep_for}')
+    sleep(max(sleep_for, 1))
